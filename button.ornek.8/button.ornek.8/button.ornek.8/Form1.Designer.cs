65cb2a926a584443f46830d935fd61e353ef822f
busramerve37 <145451633+busramerve37@users.noreply.github.com>
2023-09-25 13:49:07 +0300

    Add files via upload

---
+++ button.ornek.8/button.ornek.8/button.ornek.8/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace button.ornek._8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lstMeyve = new System.Windows.Forms.ListBox();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sepete Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 136);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lstMeyve
+            // 
+            this.lstMeyve.FormattingEnabled = true;
+            this.lstMeyve.Location = new System.Drawing.Point(318, 136);
+            this.lstMeyve.Name = "lstMeyve";
+            this.lstMeyve.Size = new System.Drawing.Size(120, 95);
+            this.lstMeyve.TabIndex = 2;
+            this.lstMeyve.SelectedIndexChanged += new System.EventHandler(this.lstMeyve_SelectedIndexChanged);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(111, 226);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(84, 50);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sepet Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 363);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.lstMeyve);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox lstMeyve;
+        private System.Windows.Forms.Button btnSil;
+    }
+}
+
